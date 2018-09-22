409cd37b413e5083f3aa06634ed3f9205b2bd6a6
BrOrlandi <brorlandi@gmail.com>
2018-09-21 22:34:34 -0300

    Remove old files.

--- src/js/_config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  apiKey: 'AIzaSyDsbhCZzdaH039531UkAJp3iD8I1Sg93A0',
-  authDomain: 'instafire-92e8f.firebaseapp.com',
-  databaseURL: 'https://instafire-92e8f.firebaseio.com',
-  projectId: 'instafire-92e8f',
-  storageBucket: 'instafire-92e8f.appspot.com',
-};
-
-export default config;
